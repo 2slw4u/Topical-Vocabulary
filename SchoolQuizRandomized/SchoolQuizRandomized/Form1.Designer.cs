f546591bef2b26553782e143c3fcdcc250a7bc55
2slw4u <56259759+2slw4u@users.noreply.github.com>
2019-12-24 18:32:39 +0700

    Add files via upload

---
+++ SchoolQuizRandomized/SchoolQuizRandomized/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace SchoolQuizRandomized
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(257, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "English";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(679, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(257, 49);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Deutsch";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(12, 116);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(924, 119);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(6, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 31);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Вопрос";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(6, 238);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 31);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ваш ответ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(12, 272);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(924, 119);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(6, 472);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 31);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Итог: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(6, 512);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(194, 31);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Верный ответ:";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(12, 415);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(924, 49);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Проверить ответ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(6, 552);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(152, 31);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Ваш ответ:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(6, 592);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(315, 31);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Соотношение ответов - ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(6, 633);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(347, 31);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Процент верных ответов - ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(6, 673);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(445, 31);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Ваша оценка на данный момент - ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(948, 734);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Приложение для Topical Vocabulary";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ SchoolQuizRandomized/SchoolQuizRandomized/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SchoolQuizRandomized
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] quest = new string[37];
+        string[] answer = new string[37];
+        int i;
+        Random R = new Random();
+        int vern;
+        int nevern;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            quest[0] = "Люди и государства доброй воли";
+            quest[1] = "Стремиться к длящемуся миру";
+            quest[2] = "Кооперироваться или объединять усилия с другими странами";
+            quest[3] = "Устраивать международные встречи, конференции, саммиты, переговоры (2 варианта) по вопросу о войне и мире";
+            quest[4] = "Подписывать международные договоры";
+            quest[5] = "Пытаться сохранить международный мир и безопасность";
+            quest[6] = "Пытаться запретить и постепенно уничтожить оружие массового поражения";
+            quest[7] = "Делать что-то так хорошо, как возможно";
+            quest[8] = "Решать локальные конфликты";
+            quest[9] = "Восстановить мир в опасных зонах";
+            quest[10] = "Предпринимать шаги к облегчению мировых волнений";
+            quest[11] = "Обещать не использовать силу в международных отношениях";
+            quest[12] = "Остановить террористические атаки";
+            quest[13] = "Бороться против терроризма";
+            quest[14] = "Спасти грядущие поколения от ужасов войны";
+            quest[15] = "Уважать права стран и личностей";
+            quest[16] = "Страны имеют право быть независимыми, иметь суверинитет и территориальную целостность";
+            quest[17] = "Личности имеют (право иметь) гражданские и политические права и свободы (два варианта)";
+            quest[18] = "Право работать";
+            quest[19] = "Право на здравоохранение";
+            quest[20] = "Право на образование";
+            quest[21] = "Право путешествовать";
+            quest[22] = "Право иметь собственность";
+            quest[23] = "Право на жизнь";
+            quest[24] = "Право на честный суд";
+            quest[25] = "Объявлены или не объявлены";
+            quest[26] = "Предоставлены или не предоставлены им";
+            quest[27] = "Соблюдены(уважаемы) или не соблюдены";
+            quest[28] = "Защищены или не защищены";
+            quest[29] = "Соблюдены или не соблюдены";
+            quest[30] = "Осуществлены или не осуществлены";
+            quest[31] = "Нарушены или не нарушены";
+            quest[32] = "Ущемлены или не ущемлены";
+            quest[33] = "Человеческое отношение к гражданским во время войны";
+            quest[34] = "Защита больных и раненых солдат";
+            quest[35] = "Справедливое обращение с пленными войны, беженцами и заложниками";
+            quest[36] = "Благодаря этим законам каждый человек может жаловаться на нарушение человеческих прав";
+
+            answer[0] = "Peoples and governments of goodwill";
+            answer[1] = "To aspire to a lasting peace";
+            answer[2] = "To cooperate or unite efforts with other countries";
+            answer[3] = "To hold international meetings, conferences, summits, negotiations, talks on problems of war and peace";
+            answer[4] = "To sign international treaties";
+            answer[5] = "To try to keep international peace and security";
+            answer[6] = "To seek to ban and gradually destroy weapons of mass destruction";
+            answer[7] = "To do one's best";
+            answer[8] = "To settle local conflicts";
+            answer[9] = "To restore peace in troubled areas";
+            answer[10] = "To take steps to ease world tensions";
+            answer[11] = "To pledge not to use force in international relations";
+            answer[12] = "To stop terrorist attacks";
+            answer[13] = "To fight against terrorism";
+            answer[14] = "To save generations to come from the horrors of war";
+            answer[15] = "To recognize the rights of countries and individuals";
+            answer[16] = "Countries are entitled to independence, sovereignty and territorial integrity";
+            answer[17] = "Individuals are entitled to civil and political rights and liberties/freedoms";
+            answer[18] = "The right to work";
+            answer[19] = "The right to health care";
+            answer[20] = "The right to education";
+            answer[21] = "The right to travel";
+            answer[22] = "The right to housing";
+            answer[23] = "The right to life";
+            answer[24] = "The right to fair trial";
+            answer[25] = "Declared or not declared";
+            answer[26] = "Provided or not provided to them";
+            answer[27] = "Respected or not respected";
+            answer[28] = "Protected or not protected";
+            answer[29] = "Observed or not observed";
+            answer[30] = "Implemented or not implemented";
+            answer[31] = "Violated or not violated";
+            answer[32] = "Abused or not abused";
+            answer[33] = "Human treatment of civilians in wartime";
+            answer[34] = "Protection for sick and wounded soldiers";
+            answer[35] = "Fair treatment of prisoners of war, refugees and hostages";
+            answer[36] = "Thanks to such laws every person can complain about human rights violation";
+
+            i = R.Next(0, 37);
+            textBox1.Text = quest[i];
+            vern = 0;
+            nevern = 0;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == answer[i])
+            {
+                label3.Text = "Итог: всё верно!";
+                label5.Text = "Ваш ответ: " + textBox2.Text;
+                label4.Text = "Верный ответ: " + answer[i];
+                vern++;
+            } else if (textBox2.Text == "last +") {
+                label3.Text = "Gottcha";
+                label5.Text = "Ваш прошлый ответ был верным";
+                label4.Text = "Верный ответ: " + answer[i];
+                vern++;
+                nevern--;
+            } else
+            {
+                label3.Text = "Итог: неверно";
+                label5.Text = "Ваш ответ: " + textBox2.Text;
+                label4.Text = "Верный ответ: " + answer[i];
+                nevern++;
+            }
+            if (nevern < 0) nevern = 0;
+            textBox2.Text = "";
+            label6.Text = "Соотношение ответов - " + vern + "/" + nevern;
+            label7.Text = "Процент верных ответов - " + (vern * 100 / (vern + nevern)) + "%";
+            i = R.Next(0, 37);
+            textBox1.Text = quest[i];
+
+            if ((vern * 100 / (vern + nevern)) >= 60 && (vern * 100 / (vern + nevern)) <= 74)
+            {
+                label8.Text = "Ваша оценка - 3. Старайтесь больше.";
+            } else if ((vern * 100 / (vern + nevern)) > 74 && (vern * 100 / (vern + nevern)) <= 89)
+            {
+                label8.Text = "Ваша оценка - 4. Очень даже неплохо, но вы сможете лучше.";
+            } else if ((vern * 100 / (vern + nevern)) > 89 )
+            {
+                label8.Text = "Ваша оценка - 5! Невероятно! Прекрасная работа!";
+            } else if ((vern * 100 / (vern + nevern)) < 60)
+            {
+                label8.Text = "Ваша оценка - 2. Либо вы ответили на малое количество вопросов, либо стоило лучше учить.";
+            }
+        }
+
+    }
+}
